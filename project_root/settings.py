4940a5b3da739dbfd64b2cb3e962e369f0f51d00
NaiNaniNai <nail02-02@mail.ru>
2024-06-07 20:50:46 +0300

    Add docker, celery and redis

--- project_root/settings.py
+++ project_root/settings.py
@@ -111,6 +111,28 @@ MIGRATION_LINTER_OPTIONS = {
 }
 
 SITE_ID = 1
+
+CASHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:16379/1",
+    }
+}
+
+if "DOCKER_ENV" in os.environ:
+    REDIS_HOST = "redis"
+else:
+    REDIS_HOST = "127.0.0.1"
+REDIS_PORT = "6379"
+CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
+CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# My variables
 CURRENT_HOST = env("CURRENT_HOST")
 CURRENT_PORT = env("CURRENT_PORT")
 SUDO_PASSWORD = env("SUDO_PASSWORD")
