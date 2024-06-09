5ea92d90d5bd37e26e6f74ca157574decb588337
NaiNaniNai <nail02-02@mail.ru>
2024-06-09 19:02:13 +0300

    Add model AnalyzedTraffic

--- ml_analyzer/models.py
+++ ml_analyzer/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from choices import LABEL
+
+
+class AnalyzedTraffic(models.Model):
+    """Model of analyzed network traffic"""
+
+    packet_rate = models.FloatField(verbose_name="Скорость передачи пакетов")
+    inter_arrival_time = models.FloatField(verbose_name="")
+    packet_size = models.FloatField(verbose_name="")
+    duration = models.FloatField(verbose_name="")
+    src_port = models.CharField(max_length=16, verbose_name="Порт источника")
+    dst_port = models.CharField(max_length=16, verbose_name="Порт назначения")
+    num_packets = models.FloatField(verbose_name="")
+    num_bytes = models.FloatField(verbose_name="")
+    protocol = models.CharField(max_length=64, verbose_name="Название протокола")
+    src_ip = models.CharField(max_length=32, verbose_name="IP источника")
+    dst_ip = models.CharField(max_length=32, verbose_name="IP назначения")
+    label = models.CharField(max_length=128, choices=LABEL, verbose_name="Вывод")
+
+    class Meta:
+        verbose_name = "Анализируемый трафик"
+        verbose_name_plural = "Анализируемые трафики"
+
+    def __str__(self):
+        return f"{self.src_ip} - {self.label}"
