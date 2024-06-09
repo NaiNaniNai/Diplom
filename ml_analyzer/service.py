e406278e56395e4ff995cdb5c8fb3161673f74a2
NaiNaniNai <nail02-02@mail.ru>
2024-06-09 19:02:54 +0300

    Refactor for pre-commit

--- ml_analyzer/service.py
+++ ml_analyzer/service.py
@@ -1,15 +1,15 @@
+import os
 import subprocess
 import threading
-import os
 from datetime import datetime
 
-from tensorflow.python import keras
+import numpy as np
+from scapy.all import rdpcap
 from scapy.layers.inet import IP, TCP, UDP
+from tensorflow.python import keras
+from tensorflow.python.keras.engine import data_adapter
 
 from project_root.settings import CURRENT_PORT, SUDO_PASSWORD
-from scapy.all import rdpcap
-import numpy as np
-from tensorflow.python.keras.engine import data_adapter
 
 
 def run_tcpdump(port, output_file, password):
@@ -97,28 +97,28 @@ def reader_file(absolute_url_save_directory, save_directory):
 def normalize_data(data):
     normal_data = []
     numeric_features = [
-        float(data['packet_rate']),
-        float(data['inter_arrival_times']),
-        float(data['packet_data'][0]['packet_size']),
-        float(data['duration']),
-        float(data['packet_data'][0]['src_port']),
-        float(data['packet_data'][0]['dst_port']),
-        float(data['num_packets']),
-        float(data['num_bytes'])
+        float(data["packet_rate"]),
+        float(data["inter_arrival_times"]),
+        float(data["packet_data"][0]["packet_size"]),
+        float(data["duration"]),
+        float(data["packet_data"][0]["src_port"]),
+        float(data["packet_data"][0]["dst_port"]),
+        float(data["num_packets"]),
+        float(data["num_bytes"]),
     ]
 
     categorical_features = [
-        hash(data['packet_data'][0]['protocol']),
-        hash(data['packet_data'][0]['src_ip']),
-        hash(data['packet_data'][0]['dst_ip'])
+        hash(data["packet_data"][0]["protocol"]),
+        hash(data["packet_data"][0]["src_ip"]),
+        hash(data["packet_data"][0]["dst_ip"]),
     ]
 
     features = numeric_features + categorical_features
     normal_data.append(features)
     normal_data = np.array(normal_data)
 
-    normal_data_mean = np.load('ml_analyzer/ml_model/normal_data_mean.npy')
-    normal_data_std = np.load('ml_analyzer/ml_model/normal_data_std.npy')
+    normal_data_mean = np.load("ml_analyzer/ml_model/normal_data_mean.npy")
+    normal_data_std = np.load("ml_analyzer/ml_model/normal_data_std.npy")
     normal_data_std[normal_data_std == 0] = 1
 
     normal_data = (normal_data - normal_data_mean) / normal_data_std
@@ -148,7 +148,9 @@ def main():
     data = reader_file(absolute_url_save_directory, save_directory)
     normal_data = normalize_data(data)
     data_adapter._is_distributed_dataset = _is_distributed_dataset
-    model_loaded = keras.models.load_model('ml_analyzer/ml_model/tf_python_keras_model2.h5')
+    model_loaded = keras.models.load_model(
+        "ml_analyzer/ml_model/tf_python_keras_model2.h5"
+    )
 
     a = model_loaded.predict(normal_data)
     print(f"Результат: {a}")
